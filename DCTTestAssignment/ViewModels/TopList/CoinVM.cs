7809241106242c94acc30466b181d9d7f1306b35
Erbium <erbium454@gmail.com>
2022-10-30 18:38:59 +0200

    top list page viewmodels added

---
+++ DCTTestAssignment/ViewModels/TopList/CoinVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTestAssignment.ViewModels
+{
+    class CoinVM
+    {
+        public int? Rank { get; set; }
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string PriceUsd { get; set; }
+    }
+}
---
+++ DCTTestAssignment/ViewModels/TopList/TopListPageVM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using DCTTestAssignment.Models;
+
+namespace DCTTestAssignment.ViewModels
+{
+    class TopListPageVM : BaseViewModel
+    {
+        private uint? top_amount;
+        public uint? TopAmount
+        {
+            get { return top_amount; }
+            set
+            {
+                top_amount = value;
+                OnPropertyChanged("TopAmount");
+            }
+        }
+
+        public IEnumerable<CoinVM>? coins;
+        public IEnumerable<CoinVM>? Coins
+        {
+            get { return coins; }
+            set
+            {
+                coins = value;
+                OnPropertyChanged("Coins");
+            }
+        }
+
+        public RelayCommand RefreshCmd { get; }
+
+        public TopListPageVM()
+        {
+            TopAmount = 10;
+            RefreshCmd = new RelayCommand(arg => Refresh());
+
+            Refresh();
+        }
+
+        private async Task Refresh()
+        {
+            var coins_dto = (await cryptoClient.GetAssets(TopAmount))?.data;
+            if (coins_dto != null)
+                Coins = coins_dto?.Select(dto => new CoinVM
+                {
+                    Rank = Int32.TryParse(dto?.rank, out int temp_rank) ? temp_rank : null,
+                    Id = dto.id,
+                    Symbol = dto.symbol,
+                    Name = dto.name,
+                    PriceUsd = dto.priceUsd
+                }).OrderBy(dto => dto.Rank);
+        }
+    }
+}
