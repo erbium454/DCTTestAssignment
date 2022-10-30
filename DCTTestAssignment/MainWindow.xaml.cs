85233afb0ca3e7bce18a5909b25cf1798ae4c448
Erbium <erbium454@gmail.com>
2022-10-30 18:42:07 +0200

    navigation request handler added

--- DCTTestAssignment/MainWindow.xaml.cs
+++ DCTTestAssignment/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,11 @@ namespace DCTTestAssignment
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LinkOnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
         }
     }
 }
