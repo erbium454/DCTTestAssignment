3a2d85222bf1a8bb52f1fcd1d0f12e4627596ef3
Erbium <erbium454@gmail.com>
2022-10-30 18:37:25 +0200

    relay command added

---
+++ DCTTestAssignment/Models/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DCTTestAssignment.Models
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object arg)
+        {
+            execute(arg);
+        }
+        public bool CanExecute(object arg)
+        {
+            return canExecute == null || canExecute(arg);
+        }
+    }
+}
