d1184bd6f854dbef1f9dd924d7911708fa36cd20
Erbium <erbium454@gmail.com>
2022-10-30 18:38:23 +0200

    base viewmodel added

---
+++ DCTTestAssignment/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DCTTestAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DCTTestAssignment.ViewModels
+{
+    abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        private CryptoClient crypto_client = null;
+        protected CryptoClient cryptoClient => crypto_client ?? (crypto_client = (CryptoClient)Application.Current.FindResource("CryptoClient"));
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
