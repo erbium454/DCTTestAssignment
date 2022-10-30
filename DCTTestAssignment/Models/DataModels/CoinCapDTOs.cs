499b7f6f470e3674b6e51ad4a1ffc2f528453e52
Erbium <erbium454@gmail.com>
2022-10-30 18:35:09 +0200

    coincap DTOs added

---
+++ DCTTestAssignment/Models/DataModels/CoinCapDTOs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTestAssignment.Models.DataModels.CoinCap
+{
+    public class Data<T>
+    {
+        public T data { get; set; }
+    }
+    public class CoinInf
+    {
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string priceUsd { get; set; }
+    }
+}
