87ae9e3d7f2637d164b4a14b722e41b575907478
Erbium <erbium454@gmail.com>
2022-11-01 02:20:01 +0200

    rest client modified. added methods for candles and rates endpoints

--- DCTTestAssignment/Models/CryptoClient.cs
+++ DCTTestAssignment/Models/CryptoClient.cs
@@ -7,6 +7,7 @@ using RestSharp;
 
 using DCTTestAssignment.Models.DataModels.CoinCap;
 using DCTTestAssignment.Models.DataModels.CoinGecko;
+using System.Windows.Markup;
 
 namespace DCTTestAssignment.Models
 {
@@ -25,5 +26,9 @@ namespace DCTTestAssignment.Models
 
         public async Task<Data<CoinInf[]>?> GetAssets(uint? limit = null) => await client.GetJsonAsync<Data<CoinInf[]>>($"{coincap_url}/assets?limit={limit?.ToString() ?? ""}");
         public async Task<CoinDetailedInf?> GetAsset(string id) => await client.GetJsonAsync<CoinDetailedInf>($"{coingecko_url}/coins/{id}");
+        public async Task<Data<CandleInf[]>?> GetCanles(string exchange, string interval, string baseId, string quoteId, long start, long end) => 
+            await client.GetJsonAsync<Data<CandleInf[]>>($"{coincap_url}/candles?exchange={exchange}&interval={interval}&baseId={baseId}&quoteId={quoteId}&start={start}&end={end}");
+        public async Task<Data<RateInf[]>?> GetRates() => await client.GetJsonAsync<Data<RateInf[]>>($"{coincap_url}/rates");
+        public async Task<Data<RateInf>?> GetRate(string id) => await client.GetJsonAsync<Data<RateInf>>($"{coincap_url}/rates/{id}");
     }
 }
