880246675e2b680278b7c90b2fbc45c850ca427f
Erbium <erbium454@gmail.com>
2022-11-01 02:18:04 +0200

    coincap DTOs models modified

--- DCTTestAssignment/Models/DataModels/CoinCapDTOs.cs
+++ DCTTestAssignment/Models/DataModels/CoinCapDTOs.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 namespace DCTTestAssignment.Models.DataModels.CoinCap
 {
-    public class Data<T>
+    class Data<T>
     {
         public T data { get; set; }
     }
-    public class CoinInf
+    class CoinInf
     {
         public string id { get; set; }
         public string rank { get; set; }
         public string symbol { get; set; }
         public string name { get; set; }
         public string priceUsd { get; set; }
+    }
+    class CandleInf
+    {
+        public string open { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string close { get; set; }
+        public string volume { get; set; }
+        public long period { get; set; }
+    }
+    class RateInf
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string currencySymbol { get; set; }
+        public string type { get; set; }
+        public string rateUsd { get; set; }
     }
 }
